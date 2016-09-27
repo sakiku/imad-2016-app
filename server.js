eb9d3f94265ed8e2453e1239769b20780080bc57
sakiku <skrikumar@gmail.com>
2016-09-27 16:01:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/ui/style.css', function (req, res) {
+app.get('/pillayar', function (req, res){
+    res.send('everything starts with this first God ! Jai vinayaga !!!');  
+ });
+app.get('/bhrama', function (req, res){
+    res.send('Bhrama is nature is to create everything !!!');  
+ }); 
+ app.get('/vishnu', function (req, res){
+    res.send('God quality to protect every being is VISHNU !!!');  
+ });
+  app.get('/SIVA', function (req, res){
+    res.send('God quality to DIMINISH every sin in to ashes is SIVA !!!');  
+ });
+ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
